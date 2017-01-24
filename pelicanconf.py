3aadc06e3a2e07ae3cee3171de7bca099051b3d0
Hyejeong Park <iam@hyejeong-park.com>
2017-01-25 01:51:06 +0900

    Install pelican

---
+++ pelicanconf.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+from datetime import date
+
+AUTHOR = 'Python Korea'
+SITENAME = 'Python Korea'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Seoul'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+STATIC_PATHS = ['images', ]
+
+THEME           = 'themes/foundation'
+
+CURRENTYEAR = date.today().year
