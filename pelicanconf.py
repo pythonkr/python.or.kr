ed996a808dd9347e91cfbdddd815c49d5eb91ce9
Hyejeong Park <iam@hyejeong-park.com>
2017-02-03 03:39:25 +0900

    Add CNAME setting

--- pelicanconf.py
+++ pelicanconf.py
@@ -35,7 +35,8 @@ DEFAULT_PAGINATION = 5
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
 
-STATIC_PATHS = ['images', ]
+STATIC_PATHS = ['images', 'extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
 
 THEME           = 'themes/foundation'
 
